d85e7ba42b70b5b32acb71ffed54c1ace484ee9b
daniel-kav <dankavatha01@gmail.com>
2023-07-28 16:39:56 +0300

    new book constructor

--- lib.js
+++ lib.js
@@ -2,4 +2,11 @@ function lib(title,author,numberOfPages,isRead){
     this.title = title ;
     this.numberOfPages = numberOfPages;
     this.isRead = isRead;
-}
+    this.info = function(){
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? "read" : "not yet read"};
+         `
+    }
+const thehobbit = new lib("The Hobbit","J.R Tolkien",295,false);
+
+console.log(thehobbit.info());
+console.log("working")
