fbbf6091fe322a7b860e329bb2accbdb763cbd04
daniel-kav <dankavatha01@gmail.com>
2023-08-09 20:39:13 +0300

    adding the new book button

--- lib.js
+++ lib.js
@@ -6,4 +6,8 @@ function Book(){
 
 function addBookToLibrary(){
 
-}
+} 
+let newBookButton = document.getElementById("new-boook-btn");
+newBookButton.addEventListener("click",function(){
+    alert("hello motherfuckre")
+})
