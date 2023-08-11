bb676245447256e86e07f9bf8d19ec155d7b3aad
daniel-kav <dankavatha01@gmail.com>
2023-08-11 21:27:19 +0300

    adding an eventlstener for the display of the new book hidden form

--- lib.js
+++ lib.js
@@ -1,14 +1,20 @@
 // lib.js
 let myLibrary = [];
-function Book(){
-   
+
+function Book(title,pages,read,author){
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+  this.author = author;
 }
-function addBookToLibrary(title,pages,read,author){
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
-    this.author = author;
+
+function addBookToLibrary(){
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').value;
 }
+
 document.addEventListener("DOMContentLoaded", function() {
     let newBookButton = document.getElementById("new-book-button");
   
