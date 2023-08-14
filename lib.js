9692c3481386978564a0d85c5d1a5712ea5f00d6
daniel-kav <dankavatha01@gmail.com>
2023-08-15 00:39:16 +0300

    config my lib array to store book objects

--- lib.js
+++ lib.js
@@ -8,6 +8,16 @@ function Book(title,pages,read,author){
   this.author = author;
 }
 
+function render(){
+  let ulEl = document.getElementById("library");
+  ulEl.innerHTML = "";
+  for(let i = 0; i < myLibrary.length; i++){
+    let liEl = document.createElement("li");
+    liEl.textContent = myLibrary[i].title;
+    liEl.textcontent = myLibrary[i].author;
+    ulEl.appendChild(liEl);
+  }
+}
 function addBookToLibrary(){
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
@@ -15,7 +25,7 @@ function addBookToLibrary(){
     let read = document.getElementById('read').checked;
     let nwBook = new Book(title, pages, read, author);
     myLibrary.push(nwBook);
-    console.log(myLibrary);
+    render();
 
 }
 
